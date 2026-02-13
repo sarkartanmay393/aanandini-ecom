7d5490db8a0f986f0ec93fc85a5a4addf4e23699
tanmay-retwin <tanmay.sarkar@re-twin.energy>
2026-02-14 04:41:02 +0530

    fix: enable S3 in production, pre-create uploads dir

--- apps/api/src/upload/storage.service.ts
+++ apps/api/src/upload/storage.service.ts
@@ -6,8 +6,7 @@ import * as path from 'path';
 @Injectable()
 export class StorageService {
     private readonly logger = new Logger(StorageService.name);
-    private readonly isProd = false;
-    //  process.env.NODE_ENV === 'production';
+    private readonly isProd = process.env.NODE_ENV === 'production';
     private s3?: S3Client;
     private bucket?: string;
 
